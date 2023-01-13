bab047a2bdb3b1bffd295363e5a7d4489f5bf1f6
Jagriti Singh <108628421+jagritisingh3456@users.noreply.github.com>
2023-01-13 17:33:38 +0530

    change

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignIn = () => {
+   const[emailErr,setEmailErr]=useState("")
+   const[passErr,setPasswordErr]=useState("")
+
+  function loginHandler(e){
+    alert("enter plz")
+    e.preventDefault()
+  }
+
+  function emailHandler(e){
+    let item=e.target.value;
+    if(item.length<3){
+      setEmailErr(true)
+    }
+    else{
+      setEmailErr(false)
+    }
+
+     console.warn(e.target.value.length)
+  }
+
+
+function passwordHandler(e){
+  let item=e.target.value;
+  if(item.length<3){
+    setPasswordErr(true)
+  }
+  else{
+    setPasswordErr(false)
+  }
+  console.warn(e.target.value)
+}
+
   return (
     <>
       <div className='heading'>
@@ -8,13 +40,13 @@ const SignIn = () => {
            <a href='/'>Forex-Rupee</a>
         </div>
         <div> 
-            <form action="#" class="">
+            <form action="#" onSubmit={loginHandler}>
             <h1>Sign in to your account</h1>
             <label for="for">Email</label>
-            <input type="email" required="" />
+            <input type="email" required="" onChange={emailHandler}/>{emailErr? <span>Not Valid</span>: ""}
             <label for="for">Password</label>
-            <input type="password" required="" />
-            <button type="submit" class="">Continue</button>
+            <input type="password" required="" onChange={passwordHandler}/>{passErr? <span>Not Valid</span>: ""}
+            <button type="submit" class="" onChange>Continue</button>
             <span>Forget Password</span>
             </form>   
         </div>
